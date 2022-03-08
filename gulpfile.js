280df10b25b260e17b6b0dc33f206a90eeb2a3a4
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-03-08 20:12:19 +0500

    fixed github pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,161 +1,146 @@
-const { src, dest, series, watch } = require('gulp');
+const {src, dest, series, watch} = require('gulp');
 const del = require('del');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
+const sass = require('gulp-dart-sass');
+const squoosh = require('gulp-squoosh');
 const uglify = require('gulp-uglify-es').default;
 const cleanCSS = require('gulp-clean-css');
-const groupMedia = require('gulp-group-css-media-queries');
 const include = require('gulp-file-include');
-const webp = require('gulp-webp');
-const webpHTML = require('gulp-webp-html');
+// const webpHTML = require('gulp-avif-webp');
 const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
 const rename = require('gulp-rename');
 const cachebust = require('gulp-cache-bust');
 const sync = require('browser-sync').create();
 const webpackStream = require('webpack-stream');
+const gulpHtmlBemValidator = require('gulp-html-bem-validator');
 
 const sourceFolder = 'app'; //папка куда собираем все исходники проекта (html, scss, js, img и т.п.)
 const buildFolder = 'docs'; //папка куда собирается проект (указываем docs, если нужен gitHubPage, дополнительно нужно указать в настройках gitHub)
 
 function html() {
-	return src([sourceFolder + '/html/**.html'])
-		.pipe(include())
-		.pipe(webpHTML())
-		.pipe(
-			cachebust({
-				type: 'timestamp',
-			}),
-		)
-		.pipe(dest(buildFolder));
+  return src([sourceFolder + '/html/**.html'])
+    .pipe(include())
+    // .pipe(webpHTML())
+    .pipe(gulpHtmlBemValidator())
+    .pipe(cachebust({
+      type: 'timestamp'
+    }))
+    .pipe(dest(buildFolder));
+};
+
+function bem() {
+  return src([sourceFolder + '/html/**.html'])
+    .pipe(gulpHtmlBemValidator())
+    .pipe(dest(buildFolder));
+};
+
+function svg() {
+  return src([sourceFolder + '/img/**/*.svg'])
+    .pipe(svgmin())
+    .pipe(dest(buildFolder + '/img'));
+};
+
+function exceptions() {
+  return src([sourceFolder + '/img/**/*.ico', sourceFolder + '/img/icons/**/*.png'])
+    .pipe(dest(buildFolder + '/img'));
 }
 
 function sprite() {
-	return src([sourceFolder + '/img/svg/**/*.svg'])
-		.pipe(
-			svgstore({
-				inlineSvg: true,
-			}),
-		)
+  return src([sourceFolder + '/img/icons/**/*.svg'])
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
 
-		.pipe(rename('sprite.svg'))
-		.pipe(dest(buildFolder + '/img/svg'));
-}
+    .pipe(rename('sprite.svg'))
+    .pipe(dest(buildFolder + '/img/icons'));
+};
 
 function scss() {
-	return src(sourceFolder + '/scss/main.scss')
-		.pipe(sass())
-		.pipe(
-			autoprefixer({
-				overrideBrowserslist: ['last 10 version'],
-				cascade: false,
-			}),
-		)
-		.pipe(groupMedia())
-		.pipe(cleanCSS({ level: 2 }))
-		.pipe(dest(buildFolder + '/css'))
-		.pipe(sass().on('error', sass.logError));
-}
+  return src(sourceFolder + '/scss/main.scss')
+    .pipe(sass())
+    .pipe(cleanCSS({level: 2}))
+    .pipe(dest(buildFolder + '/css'))
+    .pipe(sass().on('error', sass.logError));
+};
+
 
 function js() {
-	return src(sourceFolder + '/js/main.js')
-		.pipe(
-			webpackStream({
-				mode: 'none',
-				output: {
-					filename: 'main.js',
-				},
-				module: {
-					rules: [
-						{
-							test: /\.m?js$/,
-							exclude: /node_modules/,
-							use: {
-								loader: 'babel-loader',
-								options: {
-									presets: [['@babel/preset-env', { targets: 'defaults' }]],
-								},
-							},
-						},
-					],
-				},
-			}),
-		)
+  return src(sourceFolder + '/js/main.js')
+    .pipe(webpackStream({
+      mode: 'none',
+      output: {
+        filename: 'main.js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', {targets: 'defaults'}]
+                ]
+              }
+            }
+          }
+        ]
+      }
+    }))
 
-		.pipe(uglify())
-		.pipe(dest(buildFolder + '/js'));
-}
-
-function libraries() {
-	return src(sourceFolder + '/js/libraries/**.js')
-		.pipe(uglify())
-		.pipe(dest(buildFolder + '/js/libraries'));
-}
+    .pipe(uglify())
+    .pipe(dest(buildFolder + '/js'));
+};
 
 function img() {
-	return (
-		src(sourceFolder + '/img/**/*')
-			.pipe(
-				imagemin({
-					interlaced: false,
-					progressive: false,
-					optimizationLevel: 0,
-					svgoPlugins: [{ removeViewBox: false }],
-				}),
-			)
-			//раскоментрировать если нужен webp
-			.pipe(dest(buildFolder + '/img'))
-			.pipe(webp({ quality: 65 }))
-			.pipe(dest(buildFolder + '/img'))
-	);
-}
+  return src([
+    sourceFolder + '/img/**/*.jpg',
+    sourceFolder + '/img/**/*.png'
+  ])
+  //   .pipe(
+  //     squoosh(() => ({
+  //       encodeOptions: {
+  //         mozjpeg: {},
+  //         webp: {},
+  //         avif: {}
+  //       }
+  //     }))
+  //   )
+    .pipe(dest(buildFolder + '/img'));
+};
 
 function fonts() {
-	return src(sourceFolder + '/fonts/**/*').pipe(dest(buildFolder + '/fonts'));
-}
+  return src(sourceFolder + '/fonts/**/*')
+    .pipe(dest(buildFolder + '/fonts'));
+};
 
 function clear() {
-	return del(buildFolder);
-}
+  return del(buildFolder);
+};
 
 function serve() {
-	sync.init({
-		port: 3010,
-		notify: false,
-		reloadOnRestart: true,
-		server: {
-			baseDir: buildFolder,
-			directory: true,
-		},
-	});
+  sync.init({
+    port: 3010,
+    reloadOnRestart: true,
+    server: {
+      baseDir: buildFolder,
+      directory: true
+    }
+  });
 
-	watch(sourceFolder + '/html/**/*.html', series(html)).on(
-		'change',
-		sync.reload,
-	);
-	watch(sourceFolder + '/scss/**/*.scss', series(scss, html)).on(
-		'change',
-		sync.reload,
-	);
-	watch(sourceFolder + '/js/**/*.js', series(js)).on('change', sync.reload);
-	watch(sourceFolder + '/img/**/*', series(img)).on('change', sync.reload);
-	watch(sourceFolder + '/img/svg/**/*', series(sprite)).on(
-		'change',
-		sync.reload,
-	);
-	watch(sourceFolder + '/fonts/**/*', series(fonts)).on('change', sync.reload);
-}
+  watch(sourceFolder + '/html/**/*.html', series(html)).on('change', sync.reload);
+  watch(sourceFolder + '/scss/**/*.scss', series(scss, html)).on('change', sync.reload);
+  watch(sourceFolder + '/js/**/*.js', series(js)).on('change', sync.reload);
+  // watch(sourceFolder + '/img/**/*', series(img)).on('change', sync.reload);
+  watch(sourceFolder + '/img/**/*', series(svg)).on('change', sync.reload);
+  watch(sourceFolder + '/img/**/*', series(exceptions)).on('change', sync.reload);
+  watch(sourceFolder + '/img/icons/**/*', series(sprite)).on('change', sync.reload);
+  watch(sourceFolder + '/fonts/**/*', series(fonts)).on('change', sync.reload);
+};
 
-exports.build = series(clear, scss, js, libraries, img, sprite, fonts, html);
-exports.watch = series(
-	clear,
-	scss,
-	js,
-	libraries,
-	img,
-	sprite,
-	fonts,
-	html,
-	serve,
-);
+
+exports.build = series(clear, scss, js, img, exceptions, sprite, fonts, html);
+exports.watch = series(clear, scss, js, img, svg, sprite, exceptions, fonts, html, serve);
+exports.bem = bem;
 exports.clear = clear;
